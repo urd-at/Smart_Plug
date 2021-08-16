f4a2b8a311234c5dcbb479e046b830e13182ce6c
lahcene90 <lahcene90.lg@gmail.com>
2021-08-16 11:16:52 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "VpUw58GjSoxb3WVnUUSujyRZ6syu4dnJ";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Idoom 4G_BDF13";
+char pass[] = "urd2021@AT";
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+}
+
+void loop()
+{
+  Blynk.run();
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
+}
